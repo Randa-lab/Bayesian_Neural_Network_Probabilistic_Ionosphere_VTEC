7b64f798f7d6e7827a98fa823bb39f64baab14a7
Randa Natras <64141673+Randa-lab@users.noreply.github.com>
2023-04-24 14:31:51 +0200

    Update Encoding_cyclical_features.py

--- Encoding_cyclical_features.py
+++ Encoding_cyclical_features.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-#Encoding the cyclical continuous features into sine and cosine
+#Encoding the cyclical continuous features Hour and Day of year (DOY) into sine and cosine
 
-def transform_sine_cosine (data_frame, input_col_h, input_col_d,
-                           new_col_h_sin, new_col_h_cos,
-                           new_col_d_sin, new_col_d_cos):
+def encode_cyclical (data_frame, input_col_hour, input_col_doy):
   
-  data_frame = data_frame.drop([ 'DOY', 'Hour '], axis=1)
+  value_hour = 24
+  value_doy = 365.25
 
-  hour_sin = np.sin (2 * np.pi * data_frame['Hour ']/24.0)
-  hour_cos = np.cos (2 * np.pi * data_frame['Hour ']/24.0)
-  doy_sin = np.sin (2 * np.pi * data_frame['DOY']/365.25)
-  doy_cos = np.cos (2 * np.pi * data_frame['DOY']/365.25)
-
-  data_frame[new_col_h_sin] = hour_sin
-  data_frame[new_col_h_cos] = hour_cos
-  data_frame[new_col_d_sin] = doy_sin
-  data_frame[new_col_d_cos] = doy_cos
+  data_frame [input_col_hour + ' (sin)'] = np.sin (2 * np.pi * data_frame[input_col_hour] / value_hour)
+  data_frame [input_col_hour + ' (cos)'] = np.cos (2 * np.pi * data_frame[input_col_hour] / value_hour)
+  data_frame [input_col_doy + ' (sin)'] = np.sin (2 * np.pi * data_frame[input_col_doy] / value_doy)
+  data_frame [input_col_doy + ' (cos)'] = np.cos (2 * np.pi * data_frame[input_col_doy] / value_doy)
+  
+  data_frame = data_frame.drop([input_col_hour, input_col_doy], axis=1)
+  
+  
+  
+  ### encode_cyclical (data_df_2, 'Hour ', 'DOY')
